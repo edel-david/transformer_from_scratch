d073f17133a54b33fd8333d877335a14f7de5d64
high perf dist comp lecture <hpdc01@csg-headnode.ziti.uni-heidelberg.de>
2024-12-20 13:42:52 +0100

    changing run config

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 . /opt/csg/spack/share/spack/setup-env.sh
 spack env activate cuda
 spack load cuda@12.4.0
-python models/GoePT/model.py --eval-interval 4 --lr 0.04 --batch-size 24 --epochs 100 --eval-iter 50
+python models/GoePT/model.py --eval-interval 10 --lr 0.005 --batch-size 24 --epochs 201 --eval-iter 50
