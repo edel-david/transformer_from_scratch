62ef004502007ad38f2d7b87f823f7fc2cd6681e
edel-david <david.edel@gmx.de>
2025-02-14 19:15:15 +0100

    added to optuna

--- models/GoePT/train.py
+++ models/GoePT/train.py
@@ -71,165 +71,173 @@ def get_log_output_table(log_output_buffer: deque) -> Table:
     return table
 
 
-def objective(trial, args):
+def objective(trial:optuna.Trial, args):
     context_length = trial.suggest_int("context_length", 64, 1024)
     n_embd = trial.suggest_int("n_embd", 64, 512)
+    n_embd = (n_embd // 6) * 6
     n_layers = trial.suggest_int("n_layers", 2, 12)
     batch_size = args.batch_size  # use big batch size for better GPU utilization, but
     # how do we determine the biggest possible batch size?
-    lr_init = 0.05
+    
+    # formula for vram usage is aprox :
+    # batch_size * context_len * d_model * n_layers + constant
+    lr_init = args.lr
+
 
     checkpoint_dir_path = (
         args.checkpoint_dir + f"{trial.study.study_name}/{trial.number}/"
     )
     os.makedirs(checkpoint_dir_path, exist_ok=True)
+    try:
+        model = GoePT(
+            context_length=context_length,
+            n_layer=n_layers,
+            n_embd=n_embd,
+            dropout=0.1,
+            batch_size=batch_size,
+            lr=lr_init,
+        )
+        np_rng = np.random.default_rng(args.seed)
+        get_batch = partial(
+            read_datasets,
+            data_dir=args.data_dir,
+            context_length=context_length,
+            batch_size=batch_size,
+            rng=np_rng,
+        )
+        one_hot_lookup = xp.eye(8192)
+        iter_num = 0
 
-    model = GoePT(
-        context_length=context_length,
-        n_layer=n_layers,
-        n_embd=n_embd,
-        dropout=0.1,
-        batch_size=batch_size,
-        lr=lr_init,
-    )
-    np_rng = np.random.default_rng(args.seed)
-    get_batch = partial(
-        read_datasets,
-        data_dir=args.data_dir,
-        context_length=args.context_length,
-        batch_size=batch_size,
-        rng=np_rng,
-    )
-    one_hot_lookup = xp.eye(8192)
-    iter_num = 0
+        best_val_loss = 1e9
 
-    best_val_loss = 1e9
+        status_console = Console()
+        status = status_console.status("[bold green]Starting training...", spinner="runner")
+        progress_step = Progress(transient=True)
+        header_panel = Panel(Group(status, progress_step))
 
-    status_console = Console()
-    status = status_console.status("[bold green]Starting training...", spinner="runner")
-    progress_step = Progress(transient=True)
-    header_panel = Panel(Group(status, progress_step))
+        log_output_buffer = deque([], maxlen=16)
 
-    log_output_buffer = deque([], maxlen=16)
+        table_update_func = partial(
+            get_log_output_table, log_output_buffer=log_output_buffer
+        )
+        step = 0
+        # with status_console.screen():
+        with Live(header_panel):
+            while True:
+                # progress_step.console.print(f'Starting epoch: {iter_num + 1}')
+                status.update(f"[bold green]Training epoch {iter_num + 1} ...")
 
-    table_update_func = partial(
-        get_log_output_table, log_output_buffer=log_output_buffer
-    )
-    step = 0
-    # with status_console.screen():
-    with Live(header_panel):
-        while True:
-            # progress_step.console.print(f'Starting epoch: {iter_num + 1}')
-            status.update(f"[bold green]Training epoch {iter_num + 1} ...")
+                task_id = progress_step.add_task("Training")
 
-            task_id = progress_step.add_task("Training")
-
-            for micro_step in progress_step.track(
-                range(args.gradient_accumulation_steps),
-                total=args.gradient_accumulation_steps,
-                task_id=task_id,
-            ):
-
-                X, Y = get_batch("train")
-                X, Y = cp.asarray(X), cp.asarray(Y)
-                logits, loss = model.forward(X, Y, True)
-                wandb.log({"train_loss": loss.item()}, step=step)
-                # Scale the loss to account for gradient accumulation
-                loss = loss / args.gradient_accumulation_steps
-
-                # with open("train_losses.csv", "a") as f:
-                #     f.write(f"{iter_num}\t{loss:.8f}\n")
-                # disable logging into csv. Use wandb instead
-
-                # Get raw gradient
-                raw_grad, target = compute_gradient(Y, logits, one_hot_lookup)
-
-                # Continue backward
-                grad = loss * raw_grad
-
-                model.backward(grad)
-
-                log_output_buffer.append(
-                    (
-                        datetime.datetime.now().isoformat(),
-                        iter_num + 1,
-                        loss.item() * args.gradient_accumulation_steps,
-                    )
-                )
-
-                progress_step.console.clear()
-                progress_step.console.print(table_update_func())
-                progress_step.advance(task_id)
-                step += 1
-            progress_step.remove_task(task_id)
-
-            task_id = progress_step.add_task("Updating model")
-
-            model.update()
-
-            progress_step.remove_task(task_id)
-
-            # Evaluate the loss on train/val sets and write checkpoints
-
-            if iter_num % args.eval_interval == 0:
-
-                losses_val = xp.zeros(args.eval_iters)
-
-                task_id = progress_step.add_task("Val loss evaluation")
-
-                for k in progress_step.track(
-                    range(args.eval_iters), total=args.eval_iters, task_id=task_id
+                for micro_step in progress_step.track(
+                    range(args.gradient_accumulation_steps),
+                    total=args.gradient_accumulation_steps,
+                    task_id=task_id,
                 ):
 
-                    X, Y = get_batch("val")
+                    X, Y = get_batch("train")
                     X, Y = cp.asarray(X), cp.asarray(Y)
-                    logits, loss = model.forward(X, Y, False)
+                    logits, loss = model.forward(X, Y, True)
+                    wandb.log({"train_loss": loss.item()}, step=step)
+                    # Scale the loss to account for gradient accumulation
+                    loss = loss / args.gradient_accumulation_steps
 
-                    losses_val[k] = loss.item()
+                    # with open("train_losses.csv", "a") as f:
+                    #     f.write(f"{iter_num}\t{loss:.8f}\n")
+                    # disable logging into csv. Use wandb instead
 
+                    # Get raw gradient
+                    raw_grad, target = compute_gradient(Y, logits, one_hot_lookup)
+
+                    # Continue backward
+                    grad = loss * raw_grad
+
+                    model.backward(grad)
+
+                    log_output_buffer.append(
+                        (
+                            datetime.datetime.now().isoformat(),
+                            iter_num + 1,
+                            loss.item() * args.gradient_accumulation_steps,
+                        )
+                    )
+
+                    progress_step.console.clear()
+                    progress_step.console.print(table_update_func())
                     progress_step.advance(task_id)
+                    step += 1
+                progress_step.remove_task(task_id)
+
+                task_id = progress_step.add_task("Updating model")
+
+                model.update()
 
                 progress_step.remove_task(task_id)
 
-                loss_val_mean = losses_val.mean()
-                wandb.log({"val_loss": loss_val_mean.item()}, step=step)
+                # Evaluate the loss on train/val sets and write checkpoints
 
-                if loss_val_mean < best_val_loss:
+                if iter_num % args.eval_interval == 0:
 
-                    status_update_string = f"Val loss decreased from {best_val_loss:.4f} to {loss_val_mean:.4f}"
+                    losses_val = xp.zeros(args.eval_iters)
 
-                    status_update_string += ". Saving checkpoint..."
+                    task_id = progress_step.add_task("Val loss evaluation")
 
-                    status.update(status_update_string)
+                    for k in progress_step.track(
+                        range(args.eval_iters), total=args.eval_iters, task_id=task_id
+                    ):
 
-                    checkpoint_path = os.path.join(
-                        checkpoint_dir_path, f"goe_pt_iter_{iter_num}.json"
-                    )
+                        X, Y = get_batch("val")
+                        X, Y = cp.asarray(X), cp.asarray(Y)
+                        logits, loss = model.forward(X, Y, False)
 
-                    state_dict = model.state_dict()
+                        losses_val[k] = loss.item()
 
-                    with open(checkpoint_path, mode="w", encoding="utf-8") as out_file:
-                        json.dump(state_dict, out_file)
+                        progress_step.advance(task_id)
 
-                    status.update(f"Saved checkpoint under {checkpoint_path}")
+                    progress_step.remove_task(task_id)
 
-                    best_val_loss = loss_val_mean
-                trial.report(loss_val_mean.item(), iter_num)
-                if trial.should_prune():
-                    raise optuna.exceptions.TrialPruned()
-            iter_num += 1
+                    loss_val_mean = losses_val.mean()
+                    wandb.log({"val_loss": loss_val_mean.item()}, step=step)
 
-            # termination conditions
-            if iter_num > args.epochs:
-                break
-    return best_val_loss if best_val_loss != 1e9 else loss.item()
+                    if loss_val_mean < best_val_loss:
+
+                        status_update_string = f"Val loss decreased from {best_val_loss:.4f} to {loss_val_mean:.4f}"
+
+                        status_update_string += ". Saving checkpoint..."
+
+                        status.update(status_update_string)
+
+                        checkpoint_path = os.path.join(
+                            checkpoint_dir_path, f"goe_pt_iter_{iter_num}.json"
+                        )
+
+                        state_dict = model.state_dict()
+
+                        with open(checkpoint_path, mode="w", encoding="utf-8") as out_file:
+                            json.dump(state_dict, out_file)
+
+                        status.update(f"Saved checkpoint under {checkpoint_path}")
+
+                        best_val_loss = loss_val_mean
+                    trial.report(loss_val_mean.item(), iter_num)
+                    print(f"{trial.params}\ntrial asks if prune!")
+                    if trial.should_prune():
+                        raise optuna.exceptions.TrialPruned()
+                iter_num += 1
+
+                # termination conditions
+                if iter_num > args.epochs:
+                    break
+        return best_val_loss if best_val_loss != 1e9 else loss.item()
+    except cp.cuda.memory.OutOfMemoryError:
+        return 1e9
     # if epochs is smaller than eval iter,
     # best_val_loss will not be updated, so return the train loss as instead.
     # nevertheless, avoid this situation by setting epochs to a higher value than eval_iters
 
 
 def main():
-
+    wandb.init(mode="disabled")
     # Training settings
     parser = argparse.ArgumentParser(description="NanoGPT from scratch")
     parser.add_argument(
@@ -290,12 +298,14 @@ def main():
 
     study_name = "goept_language_study_1"
     storage_name = f"sqlite:///goept_{study_name}.db"
+    test_pruner = optuna.pruners.MedianPruner()
     study = optuna.create_study(
         study_name=study_name,
         storage=storage_name,
         direction="minimize",
         load_if_exists=True,
         sampler=optuna.samplers.TPESampler(),
+        pruner=optuna.pruners.SuccessiveHalvingPruner()
     )
     study.optimize(param_objective, n_trials=30)
 
